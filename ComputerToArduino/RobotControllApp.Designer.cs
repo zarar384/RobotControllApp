ff8668fd4e283238c7fce548d23a05f02bc57d6f
Felix <fel98_98@mail.ru>
2021-08-30 00:41:26 +0200

    Добавьте файлы проекта.

---
+++ ComputerToArduino/RobotControllApp.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace ComputerToArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(17, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(131, 44);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(193, 198);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(221, 91);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Connection";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(193, 48);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(221, 142);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::ComputerToArduino.Properties.Resources._3;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(17, 80);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 57);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(131, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 53);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(131, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 57);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(17, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 57);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(23, 51);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(28, 32);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(91, 51);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(28, 32);
+            this.button7.TabIndex = 12;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(57, 7);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(28, 38);
+            this.button8.TabIndex = 13;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(91, 9);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(28, 38);
+            this.button10.TabIndex = 15;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(23, 7);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(28, 38);
+            this.button11.TabIndex = 16;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button12.BackgroundImage")));
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button12.Location = new System.Drawing.Point(91, 89);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(28, 38);
+            this.button12.TabIndex = 17;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button13.BackgroundImage")));
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button13.Location = new System.Drawing.Point(23, 89);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(28, 38);
+            this.button13.TabIndex = 18;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button14.BackgroundImage")));
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button14.Location = new System.Drawing.Point(57, 89);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(28, 38);
+            this.button14.TabIndex = 19;
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button15);
+            this.groupBox4.Controls.Add(this.button9);
+            this.groupBox4.Controls.Add(this.button11);
+            this.groupBox4.Controls.Add(this.button14);
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.button13);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.button12);
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.button10);
+            this.groupBox4.Location = new System.Drawing.Point(12, 48);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(143, 241);
+            this.groupBox4.TabIndex = 20;
+            this.groupBox4.TabStop = false;
+            // 
+            // button15
+            // 
+            this.button15.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button15.BackgroundImage")));
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button15.Location = new System.Drawing.Point(23, 175);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(96, 57);
+            this.button15.TabIndex = 21;
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(57, 51);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(28, 32);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "●";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(427, 301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Robot Controll App";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ComputerToArduino/RobotControllApp.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace ComputerToArduino
+{
+
+    public partial class Form1 : Form
+
+    {
+        bool isUp = false;
+        bool isConnected = false;
+        String[] ports;
+        SerialPort port;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            disableControls();
+            getAvailableComPorts();
+            MessageBox.Show("Welcome!");
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = ports[0];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void connectToArduino()
+        {
+
+            isConnected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 1000000, Parity.None, 8, StopBits.One);
+            port.Open();
+
+            button1.Text = "STOP";
+            enableControls();
+            button1.BackColor = Color.Red;
+            label1.Text = ("Welcome!");
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            port.Write("#STOP\n");
+            port.Close();
+            button1.Text = "START";
+            disableControls();
+            button1.BackColor = Color.Lime;
+            label1.Text = ("");
+        }
+
+        private void enableControls()
+        {
+            groupBox3.Enabled = true;
+            groupBox4.Enabled = true;
+        }
+
+        private void disableControls()
+        {
+            groupBox3.Enabled = false;
+            groupBox4.Enabled = false;
+        }
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED6ON\n");
+
+            }
+            else
+            {
+                port.Write("#LED6OF\n");
+            }
+            label1.Text = ("Move left");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED3ON\n");
+            }
+            else
+            {
+                port.Write("#LED3OF\n");
+            }
+            label1.Text = ("Forward movement");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED4ON\n");
+            }
+            else
+            {
+                port.Write("#LED4OF\n");
+            }
+            label1.Text = ("Downward movement");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED7ON\n");
+            }
+            else
+            {
+                port.Write("#LED7OF\n");
+            }
+            label1.Text = ("Move to the right");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("Draw a circle");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED1ON\n");
+
+            }
+
+            else
+            {
+                port.Write("#LED1OF\n");
+            }
+            label1.Text = ("Draw a square");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED2ON\n");
+
+            }
+            else
+            {
+                port.Write("#LED2OF\n");
+            }
+            label1.Text = ("Draw a triangle");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED5ON\n");
+
+            }
+            else
+            {
+                port.Write("#LED5OF\n");
+            }
+            label1.Text = ("Stop");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED8ON\n");
+
+            }
+            else
+            {
+                port.Write("#LED8OF\n");
+            }
+            label1.Text = ("Left diagonal down");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LEDAON\n");
+
+            }
+            else
+            {
+                port.Write("#LEDAOF\n");
+            }
+            label1.Text = ("Right diagonal up");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LED9ON\n");
+
+            }
+            else
+            {
+                port.Write("#LED9OF\n");
+            }
+            label1.Text = ("Right diagonal down");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+
+                port.Write("#LEDBON\n");
+
+            }
+            else
+            {
+                port.Write("#LEDBOF\n");
+            }
+            label1.Text = ("Left diagonal up");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (!isUp)
+            {
+                toUp();
+
+                port.Write("#LEDCON\n");
+
+            }
+            else
+            {
+                toDown();
+                port.Write("#LEDDON\n");
+            }
+        }
+        private void toUp()
+        {
+            isUp = true;
+            button15.BackgroundImage = new Bitmap(@"..\12.png");
+            label1.Text = ("Down the pen");
+
+        }
+        private void toDown()
+        {
+            isUp = false;
+            button15.BackgroundImage = new Bitmap(@"C:\Users\user\Desktop\3k\Morozov\program\ass\11.png");
+            label1.Text = ("Up the pen");
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("Draw a rectangle");
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Space)
+            {
+                MessageBox.Show("Easter egg");
+            }
+        }
+
+    }
+}
